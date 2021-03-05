17ba656251558a79f1d6d3ab9e9bf22dbb7019e5
ayarihamadi <65496856+ayarihamadi@users.noreply.github.com>
2021-03-05 13:33:20 +0100

    Update resize_image.py
    Resize the extracted faces to (224,224)

--- face_extractor/resize_image.py
+++ face_extractor/resize_image.py
@@ -1,1 +1,11 @@
+import os.path
+from PIL import Image
+from tqdm import tqdm
 
+dataset_path = './drive/MyDrive/Extract/faces'
+
+for f in tqdm(os.listdir(dataset_path)):
+    f_img = dataset_path + '/' + f
+    img = Image.open(f_img)
+    img = img.resize((224, 224))
+    img.save(f_img)
